dea2ec096e79ae45f1e8994edd844dcbe27624cf
MateuszOrt <101825843+MateuszOrt@users.noreply.github.com>
2025-07-02 20:49:56 +0200

    Delete tests directory

--- tests/create_benchmark_data_test.py
+++
@@ -1,8 +0,0 @@
-from functions_for_notebook import create_benchmark_data
-
-
-def test_create_benchmark_data():
-    data, time_series, t = create_benchmark_data(data_points=100)
-    assert len(data["Value"]) == 100
-    assert len(time_series) == 100
-    assert len(t) == 100
--- tests/create_sequence_test.py
+++
@@ -1,8 +0,0 @@
-from functions_for_notebook import create_sequences, create_benchmark_data
-
-
-def test_create_sequences():
-    data, time_series, t = create_benchmark_data(data_points=100)
-    X_train, y_train = create_sequences(data["Value"], 30)
-    assert X_train.shape == (70, 30)
-    assert y_train.shape == (70,)
